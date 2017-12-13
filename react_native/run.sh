ebb43d5cda3ba15fd337d9c2f5cb0fe82d0d5c11
Steven Stanfield <stanfield@scarecrowtech.com>
2017-12-12 21:04:42 -0500

    Reorged the heirarchy to be more logical.

--- react_native/run.sh
+++ react_native/run.sh
@@ -1,16 +1,21 @@
+export VOLUMES="-v /home/sstanf/work/:/home/sstanf/work/ -v /data/sstanf/react_home/:/home/sstanf/ -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/bus/usb:/dev/bus/usb"
+
+if [ -n $SSH_AUTH_SOCK ]; then
+	export VOLUMES="$VOLUMES -v $SSH_AUTH_SOCK:/.ssh.agent"
+fi
+
 docker run \
 	--user sstanf \
 	-it \
 	--rm \
 	--net host \
-	-v /home/sstanf/work/:/home/sstanf/work/ \
-	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	--hostname REACT-NATIVE \
 	-e DISPLAY=unix$DISPLAY \
-	-v /dev/bus/usb:/dev/bus/usb \
+	-e SSH_AUTH_SOCK=/.ssh.agent \
+	--workdir /home/sstanf \
+	$VOLUMES \
 	--privileged \
 	--name react_native \
 	sstanf/react_native \
 	/bin/bash
 
-#	--cap-drop all
-#	--device /dev/usb \
