3060a6b964a41dc34219f87019767993c6caca0c
Steven Stanfield <stanfield@scarecrowtech.com>
2018-03-24 00:58:49 -0400

    Not using firefox_dev currently.

--- refresh.sh
+++ refresh.sh
@@ -3,7 +3,7 @@
 #docker rmi sstanf/flutter_website
 docker rmi sstanf/subsurface
 docker rmi sstanf/firefox
-docker rmi sstanf/firefox_dev
+#docker rmi sstanf/firefox_dev
 docker rmi sstanf/chromium
 docker rmi sstanf/browsers
 docker rmi sstanf/intellij_ui
@@ -21,7 +21,7 @@ cd archlinux && sudo ./mkimage-arch.sh && cd ..
 docker build --rm --network host -t sstanf/browsers $HOME/dockers/browsers/
 docker build --rm --network host -t sstanf/chromium $HOME/dockers/chromium/
 docker build --rm --network host -t sstanf/firefox $HOME/dockers/firefox/
-docker build --rm --network host -t sstanf/firefox_dev $HOME/dockers/firefox_dev/
+#docker build --rm --network host -t sstanf/firefox_dev $HOME/dockers/firefox_dev/
 docker build --rm --network host -t sstanf/subsurface $HOME/dockers/subsurface/
 docker build --rm --network host -t sstanf/jdk8 $HOME/dockers/jdk8/
 docker build --rm --network host -t sstanf/base_devel $HOME/dockers/base_devel/
