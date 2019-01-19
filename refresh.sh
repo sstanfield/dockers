4c7255ad80690091399ac8d13b6225c367a9e434
Steven Stanfield <stanfield@scarecrowtech.com>
2019-01-19 18:11:25 -0500

    Update refresh and add protocol devel.

--- refresh.sh
+++ refresh.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #docker rmi sstanf/flutter_website
+docker rmi sstanf/vscode
+docker rmi sstanf/node_dev
 docker rmi sstanf/subsurface
 docker rmi sstanf/firefox
 #docker rmi sstanf/firefox_dev
@@ -13,6 +15,7 @@ docker rmi sstanf/flutter
 docker rmi sstanf/react_native
 docker rmi sstanf/android_sdk
 docker rmi sstanf/neovimgtk
+docker rmi sstanf/protocol_devel
 docker rmi sstanf/base_devel
 docker rmi sstanf/jdk8
 docker rmi sstanf/archlinux
@@ -27,6 +30,7 @@ docker build --rm --network host -t sstanf/firefox $HOME/dockers/firefox/
 docker build --rm --network host -t sstanf/subsurface $HOME/dockers/subsurface/
 docker build --rm --network host -t sstanf/jdk8 $HOME/dockers/jdk8/
 docker build --rm --network host -t sstanf/base_devel $HOME/dockers/base_devel/
+docker build --rm --network host -t sstanf/protocol_devel $HOME/dockers/protocol_devel/
 docker build --rm --network host -t sstanf/android_sdk $HOME/dockers/android_sdk/
 docker build --rm --network host -t sstanf/flutter $HOME/dockers/flutter/
 docker build --rm --network host -t sstanf/react_native $HOME/dockers/react_native/
@@ -34,4 +38,6 @@ docker build --rm --network host -t sstanf/intellij $HOME/dockers/intelliJ/
 docker build --rm --network host -t sstanf/intellij_ui $HOME/dockers/intelliJ_UI/
 docker build --rm --network host -t sstanf/neovimgtk $HOME/dockers/neovimgtk/
 docker build --rm --network host -t sstanf/libreoffice $HOME/dockers/libreoffice/
+docker build --rm --network host -t sstanf/node_dev $HOME/dockers/node_dev/
+docker build --rm --network host -t sstanf/vscode $HOME/dockers/vscode/
 #docker build --rm --network host -t sstanf/flutter_website $HOME/dockers/flutter_website/
